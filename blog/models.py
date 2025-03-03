464917b49fad7b1c0c321d87d9d7958647b34e90
georgesroberto <georgesroberto21@gmail.com>
2025-03-03 21:33:02 +0300

    Add Author model and integrate with Post; update templates for author display

--- blog/models.py
+++ blog/models.py
@@ -2,11 +2,20 @@ from django.db import models
 
 
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    date_joined = models.DateField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
+    
+    
 class Post(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     date_posted = models.DateField()
     image = models.ImageField(upload_to="images/", blank=True)
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, related_name="posts", null=True)
 
     def __str__(self):
         return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 def blog_list(request):
     posts = Post.objects.all()
+    
+    paginator = Paginator(posts, 2)
+    
+    print(f"Paginator: {paginator}")
+    
+    page = request.GET.get('page')
+    
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    
 
     return render(request, "list.html", {"posts": posts})
 
