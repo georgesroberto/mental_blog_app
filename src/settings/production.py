ced2897b691af02e79c8e4c7b6e51ca0d12eb5c2
georgesroberto <georgesroberto21@gmail.com>
2025-03-13 21:17:11 +0300

    modified:   src/settings/production.py

--- src/settings/production.py
+++ src/settings/production.py
@@ -4,6 +4,7 @@ Production settings for the project.
 
 from .base import *
 from decouple import config
+import os
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config("SECRET_KEY")
@@ -13,4 +14,4 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
-STATIC_ROOT = [BASE_DIR / "staticfiles"]
+STATIC_ROOT = os.path.join(BASE_DIR / "staticfiles")
